911ed46b543db908f1a2094a04ac245361cdd223
The-Laug <54498065+The-Laug@users.noreply.github.com>
2023-03-09 12:55:48 +0100

    Task 1 Finished

--- AST.fs
+++ AST.fs
@@ -2,6 +2,53 @@
 // to store represent arithmetic expressions
 module AST
 
+
+type Arithmetic =
+    | Var of string
+    | ArrayIndex of (string*Arithmetic) // A[a]
+    | NumA of float
+    | ParsA of (Arithmetic) // (a)
+    | Times of (Arithmetic * Arithmetic)
+    | Div of (Arithmetic * Arithmetic)
+    | Plus of (Arithmetic * Arithmetic)
+    | Minus of (Arithmetic * Arithmetic)
+    | Pow of (Arithmetic * Arithmetic)
+    | UPlus of (Arithmetic)
+    | UMinus of (Arithmetic)
+
+type BooleanExpr =
+    | True // true
+    | False// false 
+    | BandB of BooleanExpr*BooleanExpr
+    | BorB of BooleanExpr*BooleanExpr
+    | BbitandB of BooleanExpr*BooleanExpr
+    | BbitorB of BooleanExpr*BooleanExpr
+    | Negation of (BooleanExpr) // ! b
+    | Equals of (Arithmetic*Arithmetic) // a = a
+    | NOTEquals of (Arithmetic*Arithmetic) // a != a
+    | LargerThan of (Arithmetic*Arithmetic) // a > a
+    | LargerOREqual of (Arithmetic*Arithmetic) // a >= a
+    | LessThan of (Arithmetic*Arithmetic) // a < a
+    | LessOREqual of (Arithmetic*Arithmetic) // a <= a
+    | ParsB of (BooleanExpr) // (b)
+    
+type Command = 
+                | Skip  // skip
+                | Sequence of (Command*Command) // C ; 
+                | GC of GuardedCommand
+                | Assign of (string*Arithmetic)
+                | AssignArray of (string*Arithmetic*Arithmetic)
+                | IfFi of GuardedCommand
+                | DoOd of GuardedCommand
+                
+and GuardedCommand = 
+    | IfThen of (BooleanExpr * Command) // b -> C
+    | ElseIf of (GuardedCommand * GuardedCommand) // GC [] GC
+
+
+
+
+
 type expr =
     | Num of float
     | TimesExpr of (expr * expr)
@@ -11,3 +58,4 @@ type expr =
     | PowExpr of (expr * expr)
     | UPlusExpr of (expr)
     | UMinusExpr of (expr)
+
--- Parse.fs
+++ Parse.fs
@@ -25,12 +25,58 @@ let parse parser src =
         eprintf "\n"
         Error(ParseError(pos, lastToken, e))
 
-let rec prettyPrint ast =
-   // TODO: start here
-   failwith "GCL parser not yet implemented"
+let rec prettyA a =
+    match a with 
+     | NumA(a) -> string a
+     | Var(a) -> a
+     | Plus(c1,c2) -> prettyA c1 + "+" + prettyA c2
+     | Minus(c1,c2) -> prettyA c1 + "-" + prettyA c2
+     | Times(c1,c2) -> prettyA c1 + "*" + prettyA c2
+     | Div(c1,c2) -> prettyA c1 + "/" + prettyA c2
+     | Pow(c1,c2) -> prettyA c1 + "^" + prettyA c2
+     | UMinus(c1) -> "-" + prettyA c1
+     | ParsA(c1) -> "(" + prettyA c1 + ")" 
+     | ArrayIndex(c1,c2) -> c1 + "[" + prettyA c2 + "]"
+     | _ -> failwith "ERROR with Arithmetic"
+and prettyBool Bool =
+    match Bool with 
+     | True -> "TRUE"
+     | False -> "FALSE"
+     | BandB(b1,b2) -> prettyBool b1 + "&&" + prettyBool b2
+     | BorB(b1,b2)  -> prettyBool b1 + "||" + prettyBool b2
+     | BbitandB(b1,b2) -> prettyBool b1 + "&" + prettyBool b2
+     | BbitorB(b1,b2) -> prettyBool b1 + "|" + prettyBool b2
+     | Negation(b1)  -> "!" + prettyBool b1 
+     | Equals(b1,b2)  -> prettyA b1 + "=" + prettyA b2
+     | NOTEquals(b1,b2) -> prettyA b1 + "!=" + prettyA b2
+     | LargerThan(b1,b2) -> prettyA b1 + ">" + prettyA b2
+     | LargerOREqual(b1,b2) -> prettyA b1 + ">=" + prettyA b2
+     | LessThan(b1,b2) -> prettyA b1 + "<" + prettyA b2
+     | LessOREqual(b1,b2) -> prettyA b1 + "<=" + prettyA b2
+     | ParsB(b1)  -> "(" + prettyBool b1 + ")"
+     | _ -> failwith "ERROR with BooleanExpr"
+and prettyGC G : string = 
+    match G with 
+     | IfThen(b,C) -> (prettyBool b) + "->" + (prettyPrint C) 
+     | ElseIf(GC1,GC2) -> (prettyGC GC1) + "\n" + "[]" + "\n" + (prettyGC GC2)
+     | _ -> failwith "ERROR with GuardedCommand"
+and prettyPrint ast =
+//    TODO: start here
+   match ast with
+     | Skip -> "skip"
+     | Sequence(c1,c2) -> prettyPrint c1 + ";\n" + prettyPrint c2
+     | GC(c1) -> (prettyGC c1)
+     | Assign(c1,c2) -> c1 + ":=" + prettyA c2
+     | AssignArray(c1,c2,c3) -> c1 + "[" + prettyA c2 + "]" + ":=" + prettyA c3
+     | IfFi(c1) -> "if " + prettyGC c1 + "\n" + "fi"
+     | DoOd(c1) -> "do " + prettyGC c1 + "\n" + "od"
+     | _ -> failwith "ERROR with Command"
+//    failwith "GCL parser not yet implemented"
+
+
 
 let analysis (src: string) : string =
-    match parse Parser.start (src) with
+    match parse Parser.startGCL (src) with
         | Ok ast ->
             Console.Error.WriteLine("> {0}", ast)
             prettyPrint ast
